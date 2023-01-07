5af7fe4f7d9892761dfda9233894a0ff4acb340b
DjusoNB3 <DjusoNB3@DESKTOP-69J7Q1A>
2023-01-07 20:17:15 +0100

    adding namespaces adding ImgPath property

--- ImagesMaui/ViewModels/MainPageViewModel.cs
+++ ImagesMaui/ViewModels/MainPageViewModel.cs
@@ -10,15 +10,24 @@ public partial class MainViewModel : BaseViewModel
     [ObservableProperty]
     string nadpis;
 
+    [ObservableProperty]
+    Uri imgPath;
+
     public MainViewModel()
     {
+
+        Nadpis = "Welcome to .Net Maui";
+
+        ImgPath = new Uri("https://aka.ms/campus.jpg");
+
         Image image = new Image
         {
-            Source = ImageSource.FromFile("lock1.png")
+            Source = ImageSource.FromUri(new Uri("https://aka.ms/campus.jpg"))// ImageSource.FromFile("lock1.png")
         };
 
         Img = image;
+        Img.Aspect = Aspect.AspectFill;
 
-        Nadpis = "Welcome to .Net Maui";
+        
     }
 }
